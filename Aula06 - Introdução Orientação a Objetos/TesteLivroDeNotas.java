d66f4376d09a10b553c1de2b485bb739a3764390
Evandro Ferraz <ferraz.evandro@outlook.com>
2025-04-04 18:17:18 -0300

    Update TesteLivroDeNotas.java

--- Aula06 - Introdução Orientação a Objetos/TesteLivroDeNotas.java
+++ Aula06 - Introdução Orientação a Objetos/TesteLivroDeNotas.java
@@ -11,16 +11,15 @@ public class TesteLivroDeNotas {
         // cria o objeto livro2 já com o valor inicial "Biologia" em seu atributo nomeDoCurso
         LivroDeNotas livro2 = new LivroDeNotas("Biologia");
         
-        // imprime o atributo nomeDoCurso dos dois objetos criados, utilizando o método get
-        // para ter acesso ao valor do atributo
+        // imprime o atributo nomeDoCurso dos dois objetos criados, utilizando o método get para ter acesso ao valor do atributo
         System.out.println("Livro1: \n " + livro1.getNomeDoCurso());
         System.out.println("Livro2: \n " + livro2.getNomeDoCurso());
         
         // pede para o usuário digitar o nome de um novo curso para o livro1
         String novoNome = JOptionPane.showInputDialog("Digite um novo nome para o curso do livro1: ");
-        // atribui o novo nome de curso para o livro1 utilizando o método set
+        // atribui o novo nome do curso para o livro1 utilizando o método set
         livro1.setNomeDoCurso(novoNome);
-        // exibe a mensagem de boas vindas do com nome do curso atualizado
+        // exibe a mensagem de boas vindas com nome do curso atualizado
         livro1.exibirMensagem();
     }
 }
